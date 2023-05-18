088a1e9e5316b51a835e544367ab55fbfd8d02ab
Ma. Donna Fidelino <102678677+Mariadonnadelossantos19@users.noreply.github.com>
2023-05-18 20:27:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById("nav-links");
+
+function showMenu() 
+{
+    navLinks.style.right = "0";
+}
+
+function hideMenu() 
+{
+    navLinks.style.right = "-200px";
+}
